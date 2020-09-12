3c8b4138374786549db99945792611219dd2d9fd
Enz0Z <enzogiova00@gmail.com>
2020-09-12 10:06:23 -0300

    feature: "S" class utils

---
+++ source/src/main/java/me/enz0z/utils/S.java
@@ -0,0 +1,12 @@
+package me.enz0z.utils;
+
+import java.util.Timer;
+
+public class S {
+
+	public static Timer T = new Timer();
+
+	public static void log(Object text) {
+		System.out.print(text.toString() + "\n");
+	}
+}
--- source/src/main/java/me/enz0z/utils/Utils.java
+++
@@ -1,8 +0,0 @@
-package me.enz0z.utils;
-
-public class Utils {
-
-	public static void print(String message) {
-		System.out.print(message + "\n");
-	}
-}
