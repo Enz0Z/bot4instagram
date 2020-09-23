be3cb764389f4529f1d0e3b350165846b48fa9cd
Enz0Z <enzogiova00@gmail.com>
2020-09-23 04:21:22 -0300

    feature: "sendFastWebhook" function

--- source/src/main/java/me/enz0z/instagram/M.java
+++ source/src/main/java/me/enz0z/instagram/M.java
@@ -1,11 +1,15 @@
 package me.enz0z.instagram;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import com.github.instagram4j.instagram4j.actions.users.UserAction;
 import com.github.instagram4j.instagram4j.models.user.Profile;
 import com.github.instagram4j.instagram4j.responses.feed.FeedUsersResponse;
+
+import me.enz0z.utils.DiscordWebhook;
+import me.enz0z.utils.Prop;
 
 public class M {
 
@@ -18,5 +22,17 @@ public class M {
 			}
 		}
 		return followers;
+	}
+	
+	public static void sendFastWebhook(String content) {
+		DiscordWebhook webhook = new DiscordWebhook(Prop.getString("Webhook"));
+		
+		webhook.setUsername("EVENT >> Error");
+		webhook.setContent(content);
+		try {
+			webhook.execute();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
