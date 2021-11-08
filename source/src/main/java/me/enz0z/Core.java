7216c38d008c6a4f36e1f239a7d6f6be2f68c1dc
Enz0Z <enzogiova00@gmail.com>
2021-11-08 20:54:20 +0100

    refactor: code cleanup

--- source/src/main/java/me/enz0z/Core.java
+++ source/src/main/java/me/enz0z/Core.java
@@ -1,11 +1,14 @@
 package me.enz0z;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-import java.util.concurrent.Callable;
 
 import com.github.instagram4j.instagram4j.IGClient;
-import com.github.instagram4j.instagram4j.IGClient.Builder.LoginHandler;
+import com.github.instagram4j.instagram4j.actions.users.UserAction;
 import com.github.instagram4j.instagram4j.exceptions.IGLoginException;
+import com.github.instagram4j.instagram4j.models.user.Profile;
+import com.github.instagram4j.instagram4j.responses.feed.FeedUsersResponse;
 import com.github.instagram4j.instagram4j.utils.IGChallengeUtils;
 
 import me.enz0z.instagram.WhoUnfollows;
@@ -14,21 +17,19 @@ import me.enz0z.utils.S;
 
 public class Core {
 
-	private static IGClient client;
+	public static IGClient client;
 
 	public static void main(String args[]) {
 		new Prop();
 		try {
-			Scanner scanner = new Scanner(System.in);
-			Callable<String> inputCode = () -> {
-			    System.out.print("Please input code: ");
-			    return scanner.nextLine();
-			};
-			LoginHandler twoFactorHandler = (client, response) -> {
-			    return IGChallengeUtils.resolveTwoFactor(client, response, inputCode);
-			};
-			client = IGClient.builder().username(Prop.getString("Username")).password(Prop.getString("Password")).onTwoFactor(twoFactorHandler).login();
+			client = IGClient.builder().username(Prop.getString("Username")).password(Prop.getString("Password")).onTwoFactor((client, response) -> {
+				return IGChallengeUtils.resolveTwoFactor(client, response, () -> {
+					Scanner scanner = new Scanner(System.in);
 
+					System.out.print("Please input code: ");
+					return scanner.nextLine();
+				});
+			}).login();
 			S.log("Currently connected as [" + client.getSelfProfile().toString() + "]");
 			WhoUnfollows.loop();
 		} catch (IGLoginException e) {
@@ -36,7 +37,14 @@ public class Core {
 		}
 	}
 
-	public static IGClient getClient() {
-		return client;
+	public static List<Profile> getFollowers(UserAction response) {
+		List<Profile> followers = new ArrayList<Profile>();
+
+		for (FeedUsersResponse follower : response.followersFeed()) {
+			for (Profile profile : follower.getUsers()) {
+				followers.add(profile);
+			}
+		}
+		return followers;
 	}
 }
--- source/src/main/java/me/enz0z/instagram/WhoUnfollows.java
+++ source/src/main/java/me/enz0z/instagram/WhoUnfollows.java
@@ -1,12 +1,9 @@
 package me.enz0z.instagram;
 
-import java.awt.Color;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.TimerTask;
 
-import com.github.instagram4j.instagram4j.IGClient;
 import com.github.instagram4j.instagram4j.models.user.Profile;
 
 import me.enz0z.Core;
@@ -16,60 +13,53 @@ import me.enz0z.utils.S;
 
 public class WhoUnfollows {
 
-	private static Boolean firstRun = true;
-	private static List<Profile> cacheFollowers = new ArrayList<Profile>();
+	private static List<Profile> cached = new ArrayList<Profile>();
 
 	public static void loop() {
-		IGClient client = Core.getClient();
-
 		S.T.schedule(new TimerTask() {
 			@Override
 			public void run() {
-				client.getActions().users().findByUsername(client.getSelfProfile().getUsername()).thenAccept(response -> {
-					if (firstRun) {
-						firstRun = false;
-						cacheFollowers = M.getFollowers(response);
+				Core.client.getActions().users().findByUsername(Core.client.getSelfProfile().getUsername()).thenAccept(response -> {
+					if (cached.size() == 0) {
+						cached = Core.getFollowers(response);
 
-						S.log("[" + S.format(S.currentSeconds()) + "] UNFOLLOWERS >> Cached " + cacheFollowers.size() + " followers.");
+						S.log("[" + S.format(S.currentSeconds()) + "] WhoUnfollows >> Cached " + cached.size() + " followers.");
 					} else {
-						List<Profile> currentFollowers = M.getFollowers(response);
+						List<Profile> followers = Core.getFollowers(response);
 
-						for (Profile cache : cacheFollowers) {
-							Boolean found = false;
+						for (Profile profile : cached) {
+							Boolean still = false;
 
-							for (Profile profile : currentFollowers) {
-								if (cache.getPk().equals(profile.getPk())) found = true;
+							for (Profile cache : followers) {
+								if (profile.getPk().equals(cache.getPk())) still = true;
 							}
-							if (!found) {
+							if (!still) {
 								DiscordWebhook webhook = new DiscordWebhook(Prop.getString("Webhook"));
 
-								webhook.setUsername("EVENT >> Unfollow");
-								webhook.addEmbed(new DiscordWebhook.EmbedObject()
-									.setColor(Color.RED)
-									.setThumbnail(cache.getProfile_pic_url())
-									.setAuthor(cache.getUsername(), "https://instagram.com/" + cache.getUsername(), cache.getProfile_pic_url())
-									.setDescription(cache.toString().length() > 2048 ? cache.toString().substring(0, 2048) : cache.toString())
-									.addField("Full Name", cache.getFull_name(), true)
-									.addField("PK", cache.getPk().toString(), true)
-									.addField("Extra Properties", cache.getExtraProperties().toString(), false)
-								);
+								webhook.setContent("`WhoUnfollows >>` https://instagram.com/" + profile.getUsername() + " unfollowed your account.");
 								try {
 									webhook.execute();
-								} catch (IOException e) {
+								} catch (Exception e) {
 									e.printStackTrace();
 								}
 							}
 						}
-						cacheFollowers = currentFollowers;
-
-						S.log("[" + S.format(S.currentSeconds()) + "] UNFOLLOWERS >> Cached " + cacheFollowers.size() + " followers.");
+						S.log("[" + S.format(S.currentSeconds()) + "] WhoUnfollows >> Cached " + followers.size() + " followers.");
+						cached = followers;
 					}
 					WhoUnfollows.loop();
 				}).exceptionally(ex -> {
-					M.sendFastWebhook("**ERROR: **`" + ex.getMessage() + "`");
-				    return null;
+					DiscordWebhook webhook = new DiscordWebhook(Prop.getString("Webhook"));
+
+					webhook.setContent("`WhoUnfollows >>` " + ex.getMessage());
+					try {
+						webhook.execute();
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					return null;
 				});
 			}
-		}, firstRun ? 0 : (S.R.nextInt(7200000-3600000) + 3600000));
+		}, cached.size() == 0 ? 0 : (S.R.nextInt(7200000 - 3600000) + 3600000));
 	}
 }
