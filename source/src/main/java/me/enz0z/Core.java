e1629f0979ff822c215a125c710e87ec8def6eac
Enz0Z <48935178+Enz0Z@users.noreply.github.com>
2022-06-05 20:51:28 +0200

    feature: save "client/cookie" to reuse the old auth token

--- source/src/main/java/me/enz0z/Core.java
+++ source/src/main/java/me/enz0z/Core.java
@@ -1,5 +1,6 @@
 package me.enz0z;
 
+import java.io.File;
 import java.time.Clock;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,7 +8,6 @@ import java.util.Scanner;
 
 import com.github.instagram4j.instagram4j.IGClient;
 import com.github.instagram4j.instagram4j.actions.users.UserAction;
-import com.github.instagram4j.instagram4j.exceptions.IGLoginException;
 import com.github.instagram4j.instagram4j.models.user.Profile;
 import com.github.instagram4j.instagram4j.responses.feed.FeedUsersResponse;
 import com.github.instagram4j.instagram4j.utils.IGChallengeUtils;
@@ -22,17 +22,26 @@ public class Core {
 	public static void main(String args[]) {
 		new Prop();
 		try {
-			client = IGClient.builder().username(Prop.getString("Username")).password(Prop.getString("Password")).onTwoFactor((client, response) -> {
-				return IGChallengeUtils.resolveTwoFactor(client, response, () -> {
-					try (Scanner scanner = new Scanner(System.in)) {
-						System.out.print("Please input code: ");
-						return scanner.nextLine();
-					}
-				});
-			}).login();
+			File clientFile = new File("./client.dmp");
+			File cookieFile = new File("./cookie.dmp");
+			
+			if (clientFile.exists() && cookieFile.exists()) {
+				client = IGClient.deserialize(clientFile, cookieFile);
+			}
+			if (client == null || (client != null && !client.isLoggedIn())) {
+				client = IGClient.builder().username(Prop.getString("Username")).password(Prop.getString("Password")).onTwoFactor((client, response) -> {
+					return IGChallengeUtils.resolveTwoFactor(client, response, () -> {
+						try (Scanner scanner = new Scanner(System.in)) {
+							System.out.print("Please input code: ");
+							return scanner.nextLine();
+						}
+					});
+				}).login();
+			}
+			client.serialize(clientFile, cookieFile);
 			System.out.println("[" + Clock.systemUTC().instant().toString() + "] Currently connected as [" + client.getSelfProfile().toString() + "]");
 			WhoUnfollows.loop();
-		} catch (IGLoginException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
